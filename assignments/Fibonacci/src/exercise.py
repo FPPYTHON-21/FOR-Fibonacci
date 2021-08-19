93a22790b109463ad5cb3c746e1b71cef7aa9867
Drabrajan <cesar.ab@culiacan.tecnm.mx>
2021-08-19 20:11:06 +0000

    Ejemplo resuelto

---
+++ assignments/Fibonacci/src/exercise.py
@@ -0,0 +1,29 @@
+import os
+def main():
+    #escribe tu código abajo de esta línea
+    os.system("clear")
+    
+    print('El sistema te arroba el valor de la serie de Fibonacci de 3 en adelante')
+    print('=======================================================================')
+    valor=int(input("Dame el número de la secuencia que buscas: "))
+    
+    Primero = 0
+    Segundo = 1
+    Tercero = 0
+    
+    if valor>2:
+        for n in range(2,valor+1):
+            Tercero = Primero + Segundo
+        
+            if n==valor:
+                print('El valor que buscas es:'+ str(Tercero))
+
+
+            Primero = Segundo
+            Segundo = Tercero
+    elif valor<=2:
+        print('Por desgracia no se aclaro que era de TRES! en adelante')    
+
+
+if __name__=='__main__':
+    main()
